17d29c71131322a06d2686489b045c8b9b961792
Simon Zhang <77755696+SimonZhang04@users.noreply.github.com>
2024-08-16 18:05:13 -0400

    uninstall icon library

--- src/components/BentoGrid.tsx
+++ src/components/BentoGrid.tsx
@@ -1,15 +1,11 @@
 import React from "react";
 import { BentoGrid, BentoGridItem } from "./ui/bento-grid";
-import {
-  IconClipboardCopy,
-  IconFileBroken,
-  IconSignature,
-  IconTableColumn,
-} from "@tabler/icons-react";
+
+import Image from "next/image";
 
 export function BentoGridTwoRow() {
   return (
-    <BentoGrid className="max-w-4xl mx-auto md:auto-rows-[20rem]">
+    <BentoGrid className="max-w-4xl mx-auto grid grid-cols-1 md:grid-cols-3 md:grid-rows-[repeat(2,_1fr)] gap-4">
       {items.map((item, i) => (
         <BentoGridItem
           key={i}
@@ -17,43 +13,55 @@ export function BentoGridTwoRow() {
           description={item.description}
           header={item.header}
           className={item.className}
-          icon={item.icon}
         />
       ))}
     </BentoGrid>
   );
 }
+
 const Skeleton = () => (
-  <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl   dark:bg-dot-white/[0.2] bg-dot-black/[0.2] [mask-image:radial-gradient(ellipse_at_center,white,transparent)]  border border-transparent dark:border-white/[0.2] bg-neutral-100 dark:bg-black"></div>
+  <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl dark:bg-dot-white/[0.2] bg-dot-black/[0.2] [mask-image:radial-gradient(ellipse_at_center,white,transparent)] border border-transparent dark:border-white/[0.2] bg-neutral-100 dark:bg-black"></div>
 );
+
 const items = [
   {
-    title: "The Dawn of Innovation",
+    title: "Bring Your Own Data",
     description: "Explore the birth of groundbreaking ideas and inventions.",
-    header: <Skeleton />,
-    className: "md:col-span-2",
-    icon: <IconClipboardCopy className="h-4 w-4 text-neutral-500" />,
+    header: <div></div>,
+    className: "md:col-span-1 md:row-span-1",
   },
   {
-    title: "The Digital Revolution",
+    title: "Create Charts",
     description: "Dive into the transformative power of technology.",
-    header: <Skeleton />,
-    className: "md:col-span-1",
-    icon: <IconFileBroken className="h-4 w-4 text-neutral-500" />,
+    header: (
+      <Image
+        src="/create-charts.png"
+        alt="Demo picture"
+        width={100} // Use a suitable width
+        height={100} // Use a suitable height
+        layout="responsive"
+        sizes="80vw"
+      />
+    ),
+    className: "md:col-span-1 md:row-span-1",
   },
   {
-    title: "The Art of Design",
+    title: "OpenBB Copilot",
+    description: "Discover the beauty of thoughtful and functional design.",
+    header: <Skeleton />,
+    className: "md:col-span-2 md:row-span-2",
+  },
+  {
+    title: "Custom Dashboard",
     description: "Discover the beauty of thoughtful and functional design.",
     header: <Skeleton />,
     className: "md:col-span-1",
-    icon: <IconSignature className="h-4 w-4 text-neutral-500" />,
   },
   {
-    title: "The Power of Communication",
+    title: "Download Reports",
     description:
       "Understand the impact of effective communication in our lives.",
     header: <Skeleton />,
-    className: "md:col-span-2",
-    icon: <IconTableColumn className="h-4 w-4 text-neutral-500" />,
+    className: "md:col-span-1",
   },
 ];
